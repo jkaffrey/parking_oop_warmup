37ac85d7149ed388a144b3f5c1a69ff1d7d17642
Jeremy <jkaffrey@gmail.com>
2016-07-28 10:05:41 -0600

    Reorganized some code

--- parking_lot.js
+++ parking_lot.js
@@ -6,33 +6,6 @@ var ParkingLot = function (spaces) {
   this.daSpaces = Array(spaces).fill('');
 };
 
-ParkingLot.prototype.compact = function () {
-
-  var compact = this.daSpaces;
-
-  for (var i = 0; i < compact.length; i++) {
-
-    var lastVal = compact.pop();
-    if (lastVal) insertFirstEmpty(compact, lastVal);
-  }
-
-  for (i = 0; i <  this.numSpot - this.compact.length; i++) {
-
-    compact.push('');
-  }
-
-  return compact;
-};
-
-ParkingLot.prototype.summary = function() {
-
-  return this.daSpaces.map(function(cVal, index, array) {
-
-    var reState = [];
-    reState.push('Position ' + (index + 1) + ((array[index]) ? ': ' + array[index] : ': (empty)'));
-    return reState.join();
-  });
-};
 
 ParkingLot.prototype.vacancies = function () {
 
@@ -49,6 +22,34 @@ ParkingLot.prototype.leave = function(plate) {
 
   this.numSpot++;
   findNNull(this.daSpaces, plate);
+};
+
+ParkingLot.prototype.summary = function() {
+
+  return this.daSpaces.map(function(cVal, index, array) {
+
+    var reState = [];
+    reState.push('Position ' + (index + 1) + ((array[index]) ? ': ' + array[index] : ': (empty)'));
+    return reState.join();
+  });
+};
+
+ParkingLot.prototype.compact = function () {
+
+  var compact = this.daSpaces;
+
+  for (var i = 0; i < compact.length; i++) {
+
+    var lastVal = compact.pop();
+    if (lastVal) insertFirstEmpty(compact, lastVal);
+  }
+
+  for (i = 0; i <  this.numSpot - this.compact.length; i++) {
+
+    compact.push('');
+  }
+
+  return compact;
 };
 
 function findNNull(array, val) {
